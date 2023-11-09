ae17a2a498171e980d6de68f8b3663069175b22a
prefer52 <tjsgh151@naver.com>
2023-11-10 02:37:45 +0900

    feat(Validator): 빈 문자열이거나 NULL인지 검증 구현

---
+++ src/main/java/christmas/validate/Validator.java
@@ -0,0 +1,10 @@
+package christmas.validate;
+
+public class Validator {
+    // 빈 문자열이거나 NULL인지 검증
+    public void validateNotEmptyString(String input) {
+        if (input == null || input.isEmpty())
+            throw new IllegalArgumentException("");
+        // 에러 메시지를 enum으로 구현
+    }
+}
