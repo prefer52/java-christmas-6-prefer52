6fb33457acd39824d4ffcff58c777d1da58f5d2c
prefer52 <tjsgh151@naver.com>
2023-11-10 03:01:40 +0900

    test(ValidatorTest): 숫자 입력 검증 테스트

---
+++ src/test/java/christmas/validate/ValidatorTest.java
@@ -0,0 +1,44 @@
+package christmas.validate;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static christmas.validate.Validator.*;
+
+class ValidatorTest {
+
+    @DisplayName("빈 문자열이거나 NULL일 경우에 대한 예외 처리")
+    @NullSource
+    @EmptySource
+    @ParameterizedTest
+    void validateNotEmptyStringTest(String input) {
+        assertThatThrownBy(() ->
+                validateNotEmptyString(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("공백 문자를 포함하는 경우에 대한 예외 처리")
+    @ValueSource(strings = {" ", "1 ", " 1", "1 2"})
+    @ParameterizedTest
+    void validateContainSpaceTest(String input) {
+        assertThatThrownBy(() ->
+                validateContainSpace(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("숫자가 아니거나 Integer보다 큰 수에 대한 예외 처리")
+    @ValueSource(strings = {"a", "!", "1a", "2147483648"})
+    @ParameterizedTest
+    void validateIntegerTest(String input) {
+        assertThatThrownBy(() ->
+                validateInteger(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
