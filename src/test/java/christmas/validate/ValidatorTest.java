3e1a2e9f8c7bdfd34d9434c0c3f2c83491bede04
prefer52 <tjsgh151@naver.com>
2023-11-12 05:26:31 +0900

    test(ValidatorTest): 메뉴 입력 검증 테스트

--- src/test/java/christmas/validate/ValidatorTest.java
+++ src/test/java/christmas/validate/ValidatorTest.java
@@ -1,6 +1,5 @@
 package christmas.validate;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -8,8 +7,10 @@ import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 import static christmas.validate.Validator.*;
 
 class ValidatorTest {
@@ -51,4 +52,69 @@ class ValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class);
 
     }
+
+    @DisplayName(",,가 포함되어 있거나 ,로 시작하거나 끝나는 것에 대한 예외 처리")
+    @ValueSource(strings = {"menu,,", ",menu", "menu,"})
+    @ParameterizedTest
+    void validateContainValidCommaTest(String input) {
+        assertThatThrownBy(() ->
+                validateContainValidComma(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("--가 포함되어 있거나, -로 시작하거나 끝나는 것에 대한 예외 처리")
+    @Test
+    void validateContainValidDashTest() {
+        List<String> inputs = List.of(new String[]{"--menu-1", "-menu-1", "menu-1-"});
+        assertThatThrownBy(() ->
+                validateContainValidDash(inputs))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("-를 기준으로 2개로 분리되지 않는 것에 대한 예외 처리")
+    @Test
+    void validateSplitSizeIsTwoTest() {
+        List<String> inputs = List.of(new String[]{"menu-1-2", "menu-2-good-3"});
+        assertThatThrownBy(() ->
+                validateSplitSizeIsTwo(inputs))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("문자열이 메뉴에 존재하는 않는 것에 대한 예외 처리")
+    @ValueSource(strings = {"김치찌개", "된장찌개", "스테이크"})
+    @ParameterizedTest
+    void validateExistMenuTest(String input) {
+        assertThatThrownBy(() ->
+                validateExistMenu(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("주문한 메뉴 중 중복되는 메뉴가 존재하는 것에 대한 예외 처리")
+    @ValueSource(strings = {"초코케이크", "아이스크림"})
+    @ParameterizedTest
+    void validateDuplicatedMenuTest(String input) {
+        Map<String, Integer> orders = Map.of("초코케이크", 1, "아이스크림", 2);
+        assertThatThrownBy(() ->
+                validateDuplicatedMenu(orders, input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("주문한 메뉴가 단 하나일 시 그게 음료인것에 대한 예외 처리")
+    @ValueSource(strings = {"제로콜라", "레드와인", "샴페인"})
+    @ParameterizedTest
+    void validateSingleMenuIsBeverageTest(String input) {
+        assertThatThrownBy(() ->
+                validateSingleMenuIsBeverage(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("전체 주문 메뉴 수가 20개를 초과하는 것에 대한 예외 처리")
+    @Test
+    void validateSumOverTest() {
+        List<Integer> inputs = List.of(1, 2, 3, 4, 5, 6);
+        assertThatThrownBy(() ->
+                validateSumOver(20, inputs))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
