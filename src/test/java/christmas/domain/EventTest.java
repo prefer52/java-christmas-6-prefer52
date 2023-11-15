1f4a4ddaa9412238da3e109636d3ff94835dfc17
prefer52 <tjsgh151@naver.com>
2023-11-15 11:09:04 +0900

    test(EventTest): 크리스마스 디데이 할인 테스트

---
+++ src/test/java/christmas/domain/EventTest.java
@@ -0,0 +1,60 @@
+package christmas.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventTest {
+    private final Order order = new Order(List.of("티본스테이크-1", "바비큐립-1", "초코케이크-2", "제로콜라-1"));
+    private Event event;
+
+    @DisplayName("크리스마스 디데이 할인 금액이 잘못 될 시 실패")
+    @CsvSource({"1, 1000", "2, 1100", "25, 3400", "26, 0"})
+    @ParameterizedTest
+    void getChristmasDDayDiscountTest(int date, int discount) {
+        event = new Event(date);
+        assertEquals(discount, event.getChristmasDDayDiscount()c/t);
+    }
+
+    @Test
+    void getWeekDayDiscountTest() {
+    }
+
+    @Test
+    void getWeekendDayDiscountTest() {
+    }
+
+    @Test
+    void getSpecialDayDiscountTest() {
+    }
+
+    @Test
+    void getComplimentaryMenuTest() {
+    }
+
+    @Test
+    void getComplimentaryEventTest() {
+    }
+
+    @Test
+    void getTotalBenefitAmountTest() {
+    }
+
+    @Test
+    void getSumOfBenefitAmountTest() {
+    }
+
+    @Test
+    void getSumOfDiscountAmountTest() {
+    }
+
+    @Test
+    void getDecemberEventBadgeTest() {
+    }
+}
